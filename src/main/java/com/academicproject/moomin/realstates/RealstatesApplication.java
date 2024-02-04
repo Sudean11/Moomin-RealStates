91f3bb5efca863b420c6c1cc099de9a4db5a9516
Sudin <sudean.mdrr@gmail.com>
2024-02-04 14:28:35 -0600

    project setup

---
+++ src/main/java/com/academicproject/moomin/realstates/RealstatesApplication.java
@@ -0,0 +1,13 @@
+package com.academicproject.moomin.realstates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealstatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealstatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academicproject/moomin/realstates/RealstatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academicproject.moomin.realstates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealstatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
