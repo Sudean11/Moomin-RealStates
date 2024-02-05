d7f2b385b2c2d00ef72bea418dd96cf91ba6d516
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2024-02-05 12:54:40 -0600

    update

--- src/main/java/com/academicproject/moomin/realstates/controller/LocationController.java
+++ src/main/java/com/academicproject/moomin/realstates/controller/LocationController.java
@@ -1,4 +1,43 @@
 package com.academicproject.moomin.realstates.Controller;
 
+import com.academicproject.moomin.realstates.entity.Location;
+import com.academicproject.moomin.realstates.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public class LocationController {
+    private final LocationService locationService;
+
+    @Autowired
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @PostMapping
+    public void createLocation(@RequestBody Location location) {
+       locationService.saveLocation(location);
+    }
+
+    @GetMapping
+    public List<Location> getAllLocations() {
+        return locationService.getAllLocations();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Location> getLocationById(@PathVariable Long id) {
+        return locationService.getLocationById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Location> updateLocation(@PathVariable Long id, @RequestBody Location locationDetails) {
+        locationService.updateLocation(id, locationDetails);
+        return ResponseEntity.ok(locationDetails);
+    }
+
+
+
 }
